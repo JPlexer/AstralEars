0378a7bb09d9c710a68ad6e1fea4f186e35f7de4
JPlexer <jplexer@omg.lol>
2021-09-19 02:31:52 +0200

    fix

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, bottoken } = require('./config.json');
+const { clientId, bottoken } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -16,7 +16,7 @@ const rest = new REST({ version: '9' }).setToken(bottoken);
 (async () => {
 	try {
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(clientId),
 			{ body: commands },
 		);
 
